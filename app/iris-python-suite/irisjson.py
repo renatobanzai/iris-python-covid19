62a64cbd0fa346f6a0f87802c9f9d370eff6dd31
renatobanzai <renatobanzai@gmail.com>
2020-05-30 17:52:35 -0300

    BANZAIRIS GLOBAL BORNS

---
+++ app/iris-python-suite/irisjson.py
@@ -0,0 +1,11 @@
+import json
+import irisnative
+
+class irisjson():
+    def __init__(self, jsonval):
+        self.jsonval = jsonval
+
+    def globallize(self, global_name):
+        print(global_name)
+
+    
